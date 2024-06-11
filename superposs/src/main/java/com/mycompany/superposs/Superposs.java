0c5b6e10f7eb74f0e956923fb67767095635ef8d
Kavindu Indumina <71215684+kavinduindumina@users.noreply.github.com>
2024-06-11 19:58:21 +0530

    Add files via upload

---
+++ superposs/src/main/java/com/mycompany/superposs/Superposs.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.superposs;
+
+/**
+ *
+ * @author kavin
+ */
+public class Superposs {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
